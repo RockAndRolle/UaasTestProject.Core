c17538d118bf639e3606557ce4195887ba5eb3c2
RockAndRolle <rolandpersson@hotmail.com>
2016-09-14 22:52:57 +0200

    Added Comment Model and Controller

---
+++ UaasTestProject.Core/Controllers/CommentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web.Mvc;
+using UaasTestProject.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace UaasTestProject.Core.Controllers
+{
+    public class CommentController : SurfaceController
+    {
+        public ActionResult Submit(CommentModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            //create comment
+            var contentService = Services.ContentService;
+            var newComment = contentService.CreateContent(model.Name, CurrentPage.Id, "Comment");
+            newComment.SetValue("firstName", model.Name);
+            newComment.SetValue("email", model.Email);
+            newComment.SetValue("message", model.Comment);
+
+            Services.ContentService.SaveAndPublishWithStatus(newComment);
+
+            TempData["success"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ UaasTestProject.Core/Models/CommentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UaasTestProject.Core.Models
+{
+    public class CommentModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
